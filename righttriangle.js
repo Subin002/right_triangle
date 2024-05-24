da21cde6f5bdb2e235fc8d1643c10796c88b7d7f
Subin002 <subindask002@gmail.com>
2024-05-24 11:24:57 +0530

    first commit

---
+++ righttriangle.js
@@ -0,0 +1,13 @@
+let n = 5;
+
+for (let i = 1; i <= n; i++) {
+    
+
+for (let j = 1; j <= n - i; j++) {
+   
+
+for (let k = 1; k < 2 - i; k++) {
+    console.log(''); break}
+    console.log('*');}
+    console.log('\n');
+}
